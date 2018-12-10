64b551919a9de327d378c2eaa42121b51127e2b2
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-12-10 18:54:27 -0500

    Initial win sig test

--- SprayFix/main.cpp
+++ SprayFix/main.cpp
@@ -1,6 +1,44 @@
 #include "GarrysMod\Lua\Interface.h"
+#include "Sigscan.h"
+#include <Windows.h>
+#include <stdio.h>
+
+#ifdef _WIN32
+const char* spray_sig = "\x55\x8B\xEC\x8B\x4D\x10\xA1????\x3B\x01\x75?\xA1????\x3B\x41\x04\x75?\xA1????"
+"\x3B\x41\x08\x74?\xF3\x0F\x10\x01\xF3\x0F\x11\x05????\xF3\x0F\x10\x41\x04\xF3\x0F\x11\x05????\xF3"
+"\x0F\x10\x41\x08\xF3\x0F\x11\x05????\x8B\x4D\x14\xA1????\x3B\x01\x75?\xA1????\x3B\x41\x04\x75?\xA1"
+"????\x3B\x41\x08\x74?\xF3\x0F\x10\x01\xF3\x0F\x11\x05????\xF3\x0F\x10\x41\x04\xF3\x0F\x11\x05????"
+"\xF3\x0F\x10\x41\x08\xF3\x0F\x11\x05????\xA1????\x8D\x55\x18\x3B\x02\x8B\xC8\xA1????\x8D\x55\x1C"
+"\x0F\x45\x4D\x18\x3B\x02\x89\x0D????\x8B\xC8\x0F\x45\x4D\x1C\xA1????\x89\x0D????\x8D\x4D\x20";
+#endif
 
 GMOD_MODULE_OPEN() {
+	HMODULE mod = LoadLibrary("server.dll");
+
+	MEMORY_BASIC_INFORMATION mem;
+
+	if (!VirtualQuery(mod, &mem, sizeof(mem)))
+		return false;
+
+	unsigned char* base_addr = (unsigned char*)mem.AllocationBase;
+
+	IMAGE_DOS_HEADER *dos = (IMAGE_DOS_HEADER*)mem.AllocationBase;
+	IMAGE_NT_HEADERS *pe = (IMAGE_NT_HEADERS*)((unsigned long)dos + (unsigned long)dos->e_lfanew);
+
+	if (pe->Signature != IMAGE_NT_SIGNATURE) {
+		return false;
+	}
+
+	size_t base_len = (size_t)pe->OptionalHeader.SizeOfImage;
+
+	printf("Loaded module: %p %i", base_addr, base_len);
+
+	void* sig = FindSignature((char*)base_addr, base_len, spray_sig);
+
+	printf("Sig result: %p", sig);
+
+	printf("\n");
+
 	return 0;
 }
 
