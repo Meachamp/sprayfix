885a0de9d90ff0227ebadfad09bc16b70e451b7a
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-12-11 23:14:22 -0500

    Cross-platform premake file

--- premake5.lua
+++ premake5.lua
@@ -1,23 +1,35 @@
 workspace "SprayFix"
-   configurations { "Debug", "Release" }
+   configurations { "Release" }
    location "build"
 
 project "SprayFix"
    kind "SharedLib"
    language "C++"
    targetdir "bin/%{cfg.buildcfg}"
-   includedirs {"../sourcesdk/common", "../sourcesdk/public/tier0", "../sourcesdk/public/tier1", "../sourcesdk/public"}
-	libdirs {"../sourcesdk/lib/public"}
-	links {"tier0", "tier1"}
-	ignoredefaultlibraries { "LIBCMT" }
-	characterset "MBCS"
-
+   includedirs {"../sourcesdk/common", "../sourcesdk/public/tier0", "../sourcesdk/public/tier1", "../sourcesdk/public", "SprayFix"}
+   architecture "x86"
    
-   files { "SprayFix/main.cpp", "SprayFix/detours/hde.cpp" }
-   
-   defines { "GMOD_USE_SOURCESDK", "WIN32" }
-   
-   targetname "gmsv_sprayfix_win32"
+   filter "system:Windows"
+      libdirs {"../sourcesdk/lib/public"}
+	  ignoredefaultlibraries { "LIBCMT" }
+	  characterset "MBCS"
+	  defines {"WIN32"}
+	  targetname "gmsv_sprayfix_win32"
+	  links {"tier0", "tier1"}
+	  
+   filter "system:Linux"
+      targetname "gmsv_sprayfix_linux"
+	  libdirs {"../sourcesdk/lib/public/linux32"}
+	  implibprefix ""
+      implibextension ""
+	  links {"tier0", ":tier1.a"}
+	  targetprefix""
+	  defines {"GNUC", "POSIX", "_LINUX", "LINUX"}
+	  buildoptions{ "-std=c++11 -fPIC -m32" }
+	  
+   targetextension ".dll"
+   files { "SprayFix/*", "SprayFix/detours/hde.cpp", "**.h" }
+   defines { "GMOD_USE_SOURCESDK"}
 
    filter "configurations:Debug"
       defines { "DEBUG" }
