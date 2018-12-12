a9701695a5a12ff913d4e07bebc2e98afc33649e
Meachamp <34523297+Meachamp@users.noreply.github.com>
2018-12-11 23:30:10 -0500

    Update makefile

--- premake5.lua
+++ premake5.lua
@@ -8,6 +8,9 @@ project "SprayFix"
    targetdir "bin/%{cfg.buildcfg}"
    includedirs {"../sourcesdk/common", "../sourcesdk/public/tier0", "../sourcesdk/public/tier1", "../sourcesdk/public", "SprayFix"}
    architecture "x86"
+   targetextension ".dll"
+   files { "SprayFix/main.cpp", "SprayFix/detours/hde.cpp", "**.h" }
+   defines { "GMOD_USE_SOURCESDK"}
    
    filter "system:Windows"
       libdirs {"../sourcesdk/lib/public"}
@@ -16,8 +19,9 @@ project "SprayFix"
 	  defines {"WIN32"}
 	  targetname "gmsv_sprayfix_win32"
 	  links {"tier0", "tier1"}
+	  system"windows"
 	  
-   filter "system:Linux"
+   filter "system:linux"
       targetname "gmsv_sprayfix_linux"
 	  libdirs {"../sourcesdk/lib/public/linux32"}
 	  implibprefix ""
@@ -26,10 +30,6 @@ project "SprayFix"
 	  targetprefix""
 	  defines {"GNUC", "POSIX", "_LINUX", "LINUX"}
 	  buildoptions{ "-std=c++11 -fPIC -m32" }
-	  
-   targetextension ".dll"
-   files { "SprayFix/*", "SprayFix/detours/hde.cpp", "**.h" }
-   defines { "GMOD_USE_SOURCESDK"}
 
    filter "configurations:Debug"
       defines { "DEBUG" }
